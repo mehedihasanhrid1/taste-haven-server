e5f6914ca587202a2f7b9a7b8a3a8912fc5a789d
mehedihasanhrid1 <mehedihasanhrid1@gmail.com>
2023-10-21 10:01:54 +0600

    New product added operation managed successfully

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lbqsrfq.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -22,6 +21,54 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    app.get('/brands', async (req, res) => {
+      try {
+        const database = client.db("TasteHaven");
+        const collection = database.collection("brands");
+        const brandsData = await collection.find({}).toArray();
+
+        res.json(brandsData);
+      } catch (error) {
+        console.error("Error fetching brand data:", error);
+        res.status(500).send("Server error");
+      }
+    });
+
+    app.post('/products/add', async (req, res) => {
+      try {
+        const database = client.db("TasteHaven");
+        const collection = database.collection("products");
+
+        const {
+          product_name,
+          image,
+          brand_name,
+          product_category,
+          price,
+          rating,
+          description
+        } = req.body;
+
+        const newProduct = {
+          product_name,
+          image,
+          brand_name,
+          product_category,
+          price,
+          rating,
+          description
+        };
+
+        const result = await collection.insertOne(newProduct);
+        res.send(result);
+        
+      } catch (error) {
+        console.error("Error adding a product:", error);
+        res.status(500).send("Server error");
+      }
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -30,12 +77,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.get('/', (req, res) => {
-    res.send('The server is running.');
+  res.send('The server is running.');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
